2a9e228f00b6e7706f8c8da47bfd031997e7e272
Jeferson2607 <jefalcazar57@gmail.com>
2025-04-28 19:48:03 -0600

    .

--- JS/scriptactividades.js
+++ JS/scriptactividades.js
@@ -228,12 +228,12 @@ pregunta13.innerHTML= '<b>4:00 PM | Remise des prix du hackathon et cérémonie 
 }
  
 document.addEventListener('DOMContentLoaded', () => {
-    if (navigator.language.startsWith('es')) {
-        establecerIdioma('español');
-    } else if (navigator.language.startsWith('en')) {
-        establecerIdioma('ingles');
-    } else if (navigator.language.startsWith('fr')) {
-        establecerIdioma('frances');
+    switch(navigator.language){
+        case 'en-US':
+            establecerIdioma('ingles')
+          break;
+
+          default:
+            break;
     }
 });
-docum
