49660fcaec9f6f37520b010d0d87ddd8c5e762d8
Jeferson2607 <jefalcazar57@gmail.com>
2025-05-05 10:51:19 -0600

    .

--- JS/scriptexpositores.js
+++ JS/scriptexpositores.js
@@ -63,6 +63,16 @@ function establecerIdioma(idioma){
         pregunta3.textContent='María Fernanda Espinoza López';
         pregunta4.textContent='Carlos Antonio Valverde Montoya';
 
+        texto1.innerHTML= 'Diseño Gráfico';
+        texto2.innerHTML= 'Diseño Gráfico';
+        texto3.innerHTML= 'Química/Ciencias Ambientales​';
+        texto4.innerHTML= 'Ciberseguridad/Tecnologías de la Información​';
+
+        subtexto1.innerHTML= 'Estudiante de último año en Colegio Técnico Don Bosco. Co-diseñó la línea gráfica oficial de Expotech 2024 (logo, carteles y elementos visuales del evento),contribuyendo a la identidad visual de la feria de ingeniería y tecnología.';
+        subtexto2.innerHTML= 'Compañero de Vargas Alemán en duodécimo año de colegio. Colaboró en el diseño de materiales gráficos para Expotech 2024 , creando afiches informativos y diseños promocionales que ilustran los proyectos presentados en la feria.';
+        subtexto3.innerHTML= 'Estudiante universitaria (UCR). Presentó un proyecto de bloqueador solar natural elaborado a partir de la broza del café (residuo del proceso del café), aprovechando desechos agrícolas para producir un filtro UV ecológico';
+        subtexto4.innerHTML= 'Estudiante universitario (TEC). Desarrolló una herramienta de análisis de redes para detectar vulnerabilidades en sistemas informáticos. Este proyecto de ciberseguridad en Expotech 2024 buscó reforzar la protección de datos y educación en seguridad digital para usuarios finales.';
+
        break;
 
        case 'ingles':
@@ -79,6 +89,17 @@ function establecerIdioma(idioma){
         pregunta2.textContent='Arath Zumbado Gaitán';
         pregunta3.textContent='María Fernanda Espinoza López';
         pregunta4.textContent='Carlos Antonio Valverde Montoya';
+
+        texto1.innerHTML = 'Graphic Design';
+        texto2.innerHTML = 'Graphic Design';
+        texto3.innerHTML = 'Chemistry/Environmental Sciences';
+        texto4.innerHTML = 'Cybersecurity/Information Technologies';
+
+        subtexto1.innerHTML = 'Senior student at Don Bosco Technical High School. Co-designed the official graphic line of Expotech 2024 (logo, posters, and visual elements of the event), contributing to the visual identity of the engineering and technology fair.';
+        subtexto2.innerHTML = 'Classmate of Vargas Alemán in twelfth grade. Collaborated in designing graphic materials for Expotech 2024, creating informational posters and promotional designs illustrating the projects presented at the fair.';
+        subtexto3.innerHTML = 'University student (UCR). Presented a natural sunscreen project made from coffee pulp (a by-product of the coffee process), using agricultural waste to produce an eco-friendly UV filter.';
+        subtexto4.innerHTML = 'University student (TEC). Developed a network analysis tool to detect vulnerabilities in IT systems. This cybersecurity project at Expotech 2024 aimed to enhance data protection and promote digital security education for end users.';
+
        
         break;
   
@@ -95,7 +116,18 @@ function establecerIdioma(idioma){
         pregunta1.textContent= 'Joel Andrés Vargas Alemán';
         pregunta2.textContent='Arath Zumbado Gaitán';
         pregunta3.textContent='María Fernanda Espinoza López';
-        pregunta4.textContent='<div class="content">Carlos Antonio Valverde Montoya holo';
+        pregunta4.textContent='Carlos Antonio Valverde Montoya';
+
+        texto1.textContent = 'Design Graphique';
+        texto2.textContent = 'Design Graphique';
+        texto3.textContent = 'Chimie/Sciences de l’Environnement';
+        texto4.textContent = 'Cybersécurité/Technologies de l’Information';
+
+        subtexto1.textContent = 'Étudiant en dernière année au Collège Technique Don Bosco. A co-conçu la ligne graphique officielle d’Expotech 2024 (logo, affiches et éléments visuels de l’événement), contribuant à l’identité visuelle de la foire d’ingénierie et de technologie.';
+        subtexto2.textContent = 'Camarade de classe de Vargas Alemán en terminale. A collaboré à la conception de supports graphiques pour Expotech 2024, en créant des affiches informatives et des visuels promotionnels illustrant les projets présentés à la foire.';
+        subtexto3.textContent = 'Étudiante universitaire (UCR). A présenté un projet d’écran solaire naturel fabriqué à partir de la pulpe de café (résidu du processus de production du café), en valorisant les déchets agricoles pour produire un filtre UV écologique.';
+        subtexto4.textContent = 'Étudiant universitaire (TEC). A développé un outil d’analyse de réseau pour détecter les vulnérabilités dans les systèmes informatiques. Ce projet de cybersécurité présenté à Expotech 2024 visait à renforcer la protection des données et à promouvoir l’éducation à la sécurité numérique pour les utilisateurs finaux.';
+
         break;
 
   
