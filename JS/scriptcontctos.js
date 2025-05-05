3432419fe3c8b35f4db840547910bdeaa14fb167
Jeferson2607 <jefalcazar57@gmail.com>
2025-05-05 10:30:31 -0600

    pendejada

--- JS/scriptcontctos.js
+++ JS/scriptcontctos.js
@@ -41,7 +41,7 @@ opcionesArray.forEach((opcion) => {
 })
 
 function establecerIdioma(idioma) {
-  idiomaActual.getElementsByTagName('img')[0].src = `/ImagenesIdiomas/${idioma}.png`;
+  idiomaActual.getElementsByTagName('img')[0].src = `../ImagenesIdiomas/${idioma}.png`;
   switch (idioma) {
     case 'español':
       titulo.textContent = 'Contacto';
--- JS/scriptexpositores.js
+++ JS/scriptexpositores.js
@@ -46,7 +46,7 @@ opcionesArray.forEach((opcion)=>{
 })
 
 function establecerIdioma(idioma){
-    idiomaActual.getElementsByTagName('img')[0].src = `/ImagenesIdiomas/${idioma}.png`;
+    idiomaActual.getElementsByTagName('img')[0].src = `../ImagenesIdiomas/${idioma}.png`;
    switch (idioma){ 
     case 'español':
         titulo.textContent= 'Expositores';
